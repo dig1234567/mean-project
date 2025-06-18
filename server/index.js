8d0b6637460d35aaf80f789fcee5120e9035879c
dig1234567 <114423104+dig1234567@users.noreply.github.com>
2025-06-18 18:38:17 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,9 +12,10 @@ const cors = require("cors");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
 // middleware
 app.use(express.json());
